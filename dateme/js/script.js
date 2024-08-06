215f11357e867d9cf77f3987f65a2f53602661cf
Ariunbold01 <rkbayaraa@gmail.com>
2024-08-06 19:37:45 +0800

    A

---
+++ dateme/js/script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const btngroup = document.querySelector('.btngroup');
+const icon = document.querySelector('.icon');
+const heart = document.querySelector('.heart');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'Yesssss ♡';
+    btngroup.classList.add('remove');
+    icon.classList.add('remove');
+    heart.classList.add('active');
+});
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
+noBtn.addEventListener('click', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
